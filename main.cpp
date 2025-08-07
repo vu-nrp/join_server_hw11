1a81ec57bc67a36ee341f54930d7cb04028aafb8
usov <spb-public@yandex.ru>
2025-08-08 01:39:15 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <boost/asio.hpp>
+// #include "async.h"
+#include "joinserver.h"
+
+//!
+//! \brief main - app entry point
+//!
+
+int main(int argc, char *argv[])
+{
+//    std::cout << "Home work #11" << std::endl;
+
+    if (argc != 2) {
+        std::cerr << "Usage: join_server <port>" << std::endl;
+        return 1;
+    }
+
+//    async::init();
+
+    try {
+        ba::io_context context;
+        JoinServer server(context, bi::tcp::endpoint(bi::tcp::v4(), std::atoi(argv[1])));
+
+        context.run();
+    } catch (std::exception &exc) {
+        std::cout << exc.what() << std::endl;
+    }
+
+//    async::deinit();
+
+    return 0;
+}
